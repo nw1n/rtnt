d20b0dbdc24884d471774a25faec0ae80edfd917
Nicolas Wanner <nicolas.th.wanner@gmail.com>
2026-08-15 01:41:02 +0200

    Revert "try"
    This reverts commit 9b1bbbbd4fe93415a03b1c61e6a67dc48e3528d0.

--- rtnt-server/src/main/java/com/example/rtnt/persistence/island/IslandMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rtnt.persistence.island;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IslandMongoRepository extends MongoRepository<IslandDocument, String> {
-}
--- rtnt-webapp/src/app/app.routes.ts
+++ rtnt-webapp/src/app/app.routes.ts
@@ -1,25 +1,9 @@
 import { Routes } from '@angular/router'
-import { IslandBrowser } from './domain/island/island-browser/island-browser'
-import { IslandMap } from './domain/island/island-map/island-map'
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'island-map',
     pathMatch: 'full',
-  },
-  {
-    path: 'island-map',
-    component: IslandMap,
-    data: {
-      title: 'Island Map',
-    },
-  },
-  {
-    path: 'island-browser',
-    component: IslandBrowser,
-    data: {
-      title: 'Island Browser',
-    },
+    children: [],
   },
 ]
