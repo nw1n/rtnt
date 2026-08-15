7cad6a33752d33760655c0a5a8e165b202bb5f86
Nicolas Wanner <nicolas.th.wanner@gmail.com>
2026-08-15 14:03:55 +0200

    refactor

--- rtnt-server/src/main/java/com/example/rtnt/cli/RecreateIslandsCommand.java
+++ rtnt-server/src/main/java/com/example/rtnt/cli/RecreateIslandsCommand.java
@@ -1,6 +1,7 @@
 package com.example.rtnt.cli;
 
 import com.example.rtnt.service.IslandService;
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -22,11 +23,15 @@ public class RecreateIslandsCommand implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) {
-        if (!args.getNonOptionArgs().contains(ARG)) {
+    public void run(@NonNull ApplicationArguments args) {
+        if (!this.isValid(args)) {
             return;
         }
         this.islandService.recreateAll();
         System.exit(SpringApplication.exit(this.context, () -> 0));
+    }
+
+    private boolean isValid(ApplicationArguments args) {
+        return args.getNonOptionArgs().contains(ARG);
     }
 }
